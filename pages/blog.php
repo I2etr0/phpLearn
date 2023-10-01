36b4779fc4dcbf61fb23ee1b64be54b442f7928c
retro_mbp <yyaroslav43@gmail.com>
2023-10-02 02:29:47 +0300

    Сделал красивый вывод большого количества текста в превью

--- pages/blog.php
+++ pages/blog.php
@@ -11,91 +11,69 @@ ini_set('display_errors', 1);
 
 <!-- Центрирование заголовка -->
 <div class="mx-auto p2 row" style="width: 200px; margin-top: 0.5%">
-    <h1>Новости</h1>
+    <h1>Статьи</h1>
 </div>
 
-
-<!-- Разделяющая полоска -->
-<!-- <div class="text-success" style="margin: 0 1% 0 1%">
-    <hr class="border border-primary border-2 opacity-50">
-</div> -->
 
 <?php
+  $check_file = '/var/www/html/news/news.txt';
 
-	$file = fopen("/var/www/html/pages/news.txt", "r+");
+  //проверка на возможность чтения файла
+  // if (is_readable($check_file)) {
+  //   echo "<div class=\"alert alert-success container text-center col-md-2 p-2 \" role=\"alert\"> Файл доступен для чтения! </div>";
+  // } else {
+  //     echo '<div class=\"alert alert-danger container text-center col-md-2 p-2 \" role=\"alert\"> Файл не доступен для чтения! </div>';
+  // }
+
+  $file = file_get_contents("/var/www/html/news/news.txt");
 	$news = explode("\n", $file);
+  echo'<div class="row" style="margin: 0 0.5% 0 0.5%">';
+  for ($i = 0; $i < count($news); $i++) {
+    $new = explode(';', $news[$i]);
+    $words = iconv_strlen($new[1]);
+    if ($words > 30){
+      echo '
+        <div class="col-sm-4 mb-sm-3">
 
-	for ($i = 0; $i < count($news); $i++) {
-		$new = explode(';', $news[$i]);
-    $title = "<h1>".$new[0]."</h1>";
-    $body = "<p>".$new[1]."</p>";
-    $date = "<p>".$new[2]."</p>";
-		
-	}
+          <div class="card">
 
+            <div class="card-body">
+              <h5 class="card-title">'.$new[0].'</h5>
+              <p class="card-text">'.trim(substr($new[1], 0, 300))."...".'</p>
+              <a href="#" class="btn btn-primary">Читать</a>
+            </div>
+
+            <div class="card-footer">
+              <small class="text-body-secondary">'.$new[2].'</small>
+            </div>
+
+          </div>
+        </div>
+      ';
+    }
+    else{
+      print '
+        <div class="col-sm-4 mb-sm-3">
+
+          <div class="card">
+
+            <div class="card-body">
+              <h5 class="card-title">'.$new[0].'</h5>
+              <p class="card-text">'.$new[1].'</p>
+              <a href="#" class="btn btn-primary">Читать</a>
+            </div>
+
+            <div class="card-footer">
+              <small class="text-body-secondary">'.$new[2].'</small>
+            </div>
+
+          </div>
+        </div>
+      ';
+    }
+  }
+  echo '</div>';
 
 ?>
-
-<!-- Карточки новостей -->
-<div class="row" style="margin: 0.5% 0.5% 0 0.5%">
-  <div class="col-sm-4 mb-3 mb-sm-3">
-    <div class="card">
-        <!-- <img src="..." class="card-img-top" alt="..."> -->
-        <div class="card-body">
-        <h5 class="card-title"><?php echo $title; ?></h5>
-        <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-        <a href="#" class="btn btn-primary">Читать</a>
-        </div>
-        <div class="card-footer">
-        <small class="text-body-secondary">Last updated</small>
-        </div>
-    </div>
-  </div>
-
-  <div class="col-sm-4 mb-3 mb-sm-3">
-    <div class="card">
-        <!-- <img src="..." class="card-img-top" alt="..."> -->
-        <div class="card-body">
-        <h5 class="card-title">Card title</h5>
-        <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-        <a href="#" class="btn btn-primary">Читать</a>
-        </div>
-        <div class="card-footer">
-        <small class="text-body-secondary">Last updated</small>
-        </div>
-    </div>
-  </div>
-
-  <div class="col-sm-4 mb-3 mb-sm-3">
-    <div class="card">
-        <!-- <img src="..." class="card-img-top" alt="..."> -->
-        <div class="card-body">
-        <h5 class="card-title">Card title</h5>
-        <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-        <a href="#" class="btn btn-primary">Читать</a>
-        </div>
-        <div class="card-footer">
-        <small class="text-body-secondary">Last updated</small>
-        </div>
-    </div>
-  </div>
-
-
-  <div class="col-sm-4 mb-3 mb-sm-3">
-    <div class="card">
-        <!-- <img src="..." class="card-img-top" alt="..."> -->
-        <div class="card-body">
-        <h5 class="card-title">Card title</h5>
-        <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-        <a href="#" class="btn btn-primary">Читать</a>
-        </div>
-        <div class="card-footer">
-        <small class="text-body-secondary">Last updated</small>
-        </div>
-    </div>
-  </div>
-</div>
-
-
 
 </body>
